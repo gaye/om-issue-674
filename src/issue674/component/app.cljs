0de9a71424a8b762283fc9356cc65d147bfcb7cf
gaye <gareth.aye@gmail.com>
2016-05-04 10:57:53 -0700

    Make input child of app

--- src/issue674/component/app.cljs
+++ src/issue674/component/app.cljs
@@ -1,16 +1,9 @@
 (ns issue674.component.app
-  (:require [om.dom :as dom]
+  (:require [issue674.component.input :as input]
+            [om.dom :as dom]
             [om.next :as om :refer-macros [defui]]))
 
 (defui App
-  static om/IQuery
-  (query [this] [:text])
-
   Object
   (render [this]
-    (defn handle-change [event]
-      (om/transact! this `[(app/update-text {:value ~event.target.value})]))
-    (let [{:keys [text]} (om/props this)]
-      (dom/input #js {:type "text"
-                      :value text
-                      :onChange handle-change}))))
+    (input/input)))
